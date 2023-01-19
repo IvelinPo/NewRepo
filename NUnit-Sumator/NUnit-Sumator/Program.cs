4fb81de925c07b75e9783e03ed6b7fdefa952c2a
ivelin popov <ivelin.popov@theopentag.com>
2023-01-19 19:52:36 +0200

    Add project files.

---
+++ NUnit-Sumator/NUnit-Sumator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NUnit_Sumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
