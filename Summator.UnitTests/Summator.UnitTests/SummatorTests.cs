508a32a1eed85078ba67bb9c1b668be5147ee7c0
ivelin popov <ivelin.popov@theopentag.com>
2023-01-20 00:09:41 +0200

    Init repo

---
+++ Summator.UnitTests/Summator.UnitTests/SummatorTests.cs
@@ -0,0 +1,16 @@
+namespace Summator.UnitTests
+{
+    public class SummatorTests
+    {
+        public void Setup()
+        {
+            [Test]
+            public void Test_Summator_SumTwoPositiveNumbers()
+            {
+                var nums = new int[] { 1, 2 };
+                var actial = 
+            }
+        }
+
+    }
+}
